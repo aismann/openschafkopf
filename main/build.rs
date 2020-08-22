ca2d8b079b71c370b8bff9216869d05036d487d6
philipp <descpl@yahoo.de>
2020-08-22 23:33:32 +0200

    Somewhat simplify build.rs
    This exploits that - as of now - only external commands are only needed to
    generate files.

--- main/build.rs
+++ main/build.rs
@@ -7,7 +7,8 @@ use std::{env, fs::File, io::Write, path::Path, process::Command};
 
 fn main() {
     // adapted from https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
-    let execute_external = |cmd: &mut Command| {
+    let execute_external = |path_in: &Path, cmd: &mut Command| {
+        println!("cargo:rerun-if-changed={}", debug_verify!(path_in.to_str()).unwrap());
         let output = debug_verify!(cmd.output()).unwrap();
         assert!(output.status.success(), "{:?}: {:?}", cmd, output);
         output
@@ -17,26 +18,27 @@ fn main() {
     let path_out_dir = Path::new(&str_env_var_out_dir); // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
     // TODO can we avoid lessc depencency?
     let path_css_in = path_resources.join("css.less");
-    println!("cargo:rerun-if-changed={}", debug_verify!(path_css_in.to_str()).unwrap());
     debug_verify!(
         debug_verify!(
             File::create(&path_out_dir.join("css.css"))
         ).unwrap()
             .write_all(&execute_external(
+                &path_css_in,
                 Command::new("lessc")
-                    .arg(path_css_in)
+                    .arg(&path_css_in)
             ).stdout)
     ).unwrap();
     // TODO can we avoid inkscape depencency?
     let path_svg_in = path_resources.join("cards.svg");
-    println!("cargo:rerun-if-changed={}", debug_verify!(path_svg_in.to_str()).unwrap());
     execute_external(
+        &path_svg_in,
         Command::new("inkscape")
             .arg(&path_svg_in)
             .arg(format!("--export-filename={}", debug_verify!(path_out_dir.join("cards.png").to_str()).unwrap()))
     );
     let path_svg_3dpi = path_out_dir.join("cards.png");
     execute_external(
+        &path_svg_in,
         Command::new("inkscape")
             .arg(&path_svg_in)
             .arg(format!("--export-filename={}", debug_verify!(path_svg_3dpi.to_str()).unwrap()))
