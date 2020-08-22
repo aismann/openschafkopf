03b1a5938dac25a3f27ccc80f02886afe246171f
philipp <descpl@yahoo.de>
2020-08-22 23:24:53 +0200

    build.rs generates PNGs for site

--- main/build.rs
+++ main/build.rs
@@ -1,4 +1,7 @@
 use openschafkopf_util::*;
+use image;
+use image::GenericImageView;
+use as_num::*;
 
 use std::{env, fs::File, io::Write, path::Path, process::Command};
 
@@ -29,8 +32,42 @@ fn main() {
     println!("cargo:rerun-if-changed={}", debug_verify!(path_svg_in.to_str()).unwrap());
     execute_external(
         Command::new("inkscape")
-            .arg(path_svg_in)
+            .arg(&path_svg_in)
             .arg(format!("--export-filename={}", debug_verify!(path_out_dir.join("cards.png").to_str()).unwrap()))
     );
+    let path_svg_3dpi = path_out_dir.join("cards.png");
+    execute_external(
+        Command::new("inkscape")
+            .arg(&path_svg_in)
+            .arg(format!("--export-filename={}", debug_verify!(path_svg_3dpi.to_str()).unwrap()))
+            .arg(format!("--export-dpi={}", 3*/*default DPI*/96))
+    );
+    let img = /*TODO debug_verify!*/image::open(path_svg_3dpi).unwrap();
+    let (n_width, n_height) = img.dimensions();
+    let str_efarbe = "EGHS";
+    let str_eschlag = "AZKOU987";
+    assert_eq!(n_width % str_eschlag.len().as_num::<u32>(), 0);
+    let n_width_card = n_width / str_eschlag.len().as_num::<u32>();
+    assert_eq!(n_height % str_efarbe.len().as_num::<u32>(), 0);
+    let n_height_card = n_height / str_efarbe.len().as_num::<u32>();
+    for (i_efarbe, ch_efarbe) in str_efarbe.chars().enumerate() {
+        for (i_eschlag, ch_eschlag) in str_eschlag.chars().enumerate() {
+            debug_verify!(
+                img.view(
+                    /*x*/n_width_card * i_eschlag.as_num::<u32>(),
+                    /*y*/n_height_card * i_efarbe.as_num::<u32>(),
+                    n_width_card,
+                    n_height_card,
+                )
+                    .to_image()
+                    .save(
+                        path_resources // TODO allowed to write into this directory?
+                            .join("site")
+                            .join("img")
+                            .join(format!("{}{}.png", ch_efarbe, ch_eschlag))
+                    )
+            ).unwrap();
+        }
+    }
 }
 
