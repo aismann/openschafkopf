2358618cb5e98b25022cec1d1d614c72cdafe6cc
philipp <descpl@yahoo.de>
2020-10-07 13:15:41 +0200

    Customizable suggest-card (7.2) fewer uses of determinebestcard (2) rules

--- main/src/subcommands/suggest_card.rs
+++ main/src/subcommands/suggest_card.rs
@@ -38,7 +38,7 @@ pub fn suggest_card(
                 $itahand,
                 $func_filter_allowed_cards,
                 &$foreachsnapshot::new(
-                    determinebestcard.rules,
+                    rules,
                     epi_fixed,
                     /*tpln_stoss_doubling*/(0, 0), // TODO? make customizable
                     /*n_stock*/0, // TODO? make customizable
@@ -65,10 +65,10 @@ pub fn suggest_card(
         cartesian_match!(
             forward,
             match ((oiteratehands, eremainingcards)) {
-                (Some(All), _)|(None, _1)|(None, _2)|(None, _3)|(None, _4) => (all_possible_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, determinebestcard.rules)),
-                (Some(Sample(n_samples)), _) => (forever_rand_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, determinebestcard.rules)
+                (Some(All), _)|(None, _1)|(None, _2)|(None, _3)|(None, _4) => (all_possible_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, rules)),
+                (Some(Sample(n_samples)), _) => (forever_rand_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, rules)
                     .take(n_samples)),
-                (None, _5)|(None, _6)|(None, _7)|(None, _8) => (forever_rand_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, determinebestcard.rules)
+                (None, _5)|(None, _6)|(None, _7)|(None, _8) => (forever_rand_hands(determinebestcard.stichseq, determinebestcard.hand_fixed.clone(), epi_fixed, rules)
                     .take(/*n_suggest_card_samples*/50)),
             },
             match ((otpln_branching_factor, eremainingcards)) {
